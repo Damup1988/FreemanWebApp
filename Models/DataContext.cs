0e48855940fb6e054fbd2f90048b210eeb14ae65
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-04-17 00:10:39 +0300

    Chapter 18 - Creating the Data Model

---
+++ Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        
+        public DataContext(DbContextOptions<DataContext> opts) : base(opts)
+        {
+            
+        }
+    }
+}
