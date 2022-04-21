03c1f7e329f93f78ef270e9e225f095547fbaea5
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-04-21 22:43:32 +0300

    Chapter 20 - Understanding the Default Content Policy

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContentController : ControllerBase
+    {
+        private DataContext _dataContext;
+
+        public ContentController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public async Task<Product> GetObject()
+        {
+            return await _dataContext.Products.FirstAsync();
+        }
+    }
+}
