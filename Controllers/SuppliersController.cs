48aa05ae0d70b13731db05c2624a8908edb12ffa
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-04-21 16:45:54 +0300

    Chapter 20 - Preparing for This Chapter

---
+++ Controllers/SuppliersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuppliersController : ControllerBase
+    {
+        private DataContext _dataContext;
+
+        public SuppliersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<Supplier> GetSupplierAsync(long id)
+        {
+            return await _dataContext.Suppliers.FindAsync(id);
+        }
+    }
+}
