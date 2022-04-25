523c1b212d0b1b31b32caf487ac2853d2cd59844
Damir Safarov <damup1988@gmail.com>
2022-04-26 00:07:13 +0300

    Chapter 21 - Creating an HTML Controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(long id = 1)
+        {
+            return View(await _context.Products.FindAsync(id));
+        }
+    }
+}
