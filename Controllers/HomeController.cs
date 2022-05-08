fbefa0114e6bc55825f88350a6cb8102786d1671
Damir Safarov <damup1988@gmail.com>
2022-05-08 14:06:43 +0300

    Chapter 22 - Using the View Bag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -15,6 +16,7 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
+            ViewBag.AveragePrice = await _context.Products.AverageAsync(p => p.Price);
             return View(await _context.Products.FindAsync(id));
         }
 
