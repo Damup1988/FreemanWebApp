984ed2d460fc8604231b82c3f5b18a07469d0b9c
Damir Safarov <damup1988@gmail.com>
2022-05-13 15:23:35 +0300

    Chapter 22 - Using Temp Data

---
+++ Controllers/CubeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CubedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Cubed");
+        }
+
+        public IActionResult Cube(double num)
+        {
+            TempData["value"] = num.ToString(CultureInfo.InvariantCulture);
+            TempData["result"] = Math.Pow(num, 3).ToString(CultureInfo.InvariantCulture);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
